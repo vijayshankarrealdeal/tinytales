1c1f99d888580314d18c93a957dd2c4da3c0ac1f
Vijay Shankar <vijay.shankar@msci.com>
2025-04-12 16:54:06 +0530

    allow cross origin

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 from routes.api_router import router
 
 app = FastAPI()
+
+# Allow origins (adjust for production)
+origins = [
+    "https://tinytales-1847a.web.app/",  # <-- for development; in production, use your frontend URL
+    # "https://your-flutter-app.web.app",
+    # "https://tinytales.web.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],    # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],    # Authorization, Content-Type, etc.
+)
+
 
 app.include_router(router)
 

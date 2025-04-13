05ed34eca8dc34aa2f34406994bfb5ac25b2dac2
Vijay Shankar <vijay.shankar@msci.com>
2025-04-13 15:57:44 +0530

    password length to match 6 or more

--- models/auth_model.py
+++ models/auth_model.py
@@ -8,15 +8,13 @@ class UserBase(BaseModel):
     
 class UserRegister(UserBase):
     fullname: str
-    password: str 
-    #= Field(..., min_length=6, max_length=50)
+    password: str = Field(..., min_length=6, max_length=50)
 
     class Config:
         orm_mode = True
 
 class UserLogin(UserBase):
-    password: str 
-    #= Field(..., min_length=6, max_length=50)
+    password: str = Field(..., min_length=6, max_length=50)
 
     class Config:
         orm_mode = True

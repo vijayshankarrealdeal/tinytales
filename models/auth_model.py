1e4cbe76b4902316fc3b8d72f269825f1ed2e829
Vijay Shankar <vijay.shankar@msci.com>
2025-04-13 16:17:09 +0530

    fixed error message

--- models/auth_model.py
+++ models/auth_model.py
@@ -1,11 +1,22 @@
 from typing import Optional
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator, validator
+from pydantic_core import PydanticCustomError
 
 
 class UserBase(BaseModel):
     is_new_user: bool = Field(default=False)
-    email: EmailStr 
-    
+    email: EmailStr
+
+    @field_validator("email")
+    def custom_email_error(cls, value: str):
+        try:
+            return EmailStr.validate(value)
+        except ValueError:
+            raise PydanticCustomError(
+                "value_error.email", "Please provide a valid email address."
+            )
+
+
 class UserRegister(UserBase):
     fullname: str
     password: str = Field(..., min_length=6, max_length=50)
@@ -13,18 +24,22 @@ class UserRegister(UserBase):
     class Config:
         orm_mode = True
 
+
 class UserLogin(UserBase):
     password: str = Field(..., min_length=6, max_length=50)
 
     class Config:
         orm_mode = True
 
+
 class UserResponse(BaseModel):
     fullname: str
     email: EmailStr
     is_new_user: bool
+
     class Config:
         orm_mode = True
+
 
 class UserUpdate(BaseModel):
     fullname: Optional[str] = None
@@ -33,5 +48,6 @@ class UserUpdate(BaseModel):
     email: Optional[EmailStr] = None
     gender: Optional[str] = None
     dob: Optional[str] = None
+
     class Config:
         orm_mode = True

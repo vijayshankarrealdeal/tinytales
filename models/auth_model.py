8d8f1117a217b44c782c84374b75ea36f6aefd0c
Vijay Shankar <vijay.shankar@msci.com>
2025-04-13 16:02:49 +0530

    add email and password support to real

--- models/auth_model.py
+++ models/auth_model.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
 
 class UserBase(BaseModel):
     is_new_user: bool = Field(default=False)
-    email: str 
+    email: EmailStr 
     
 class UserRegister(UserBase):
     fullname: str
@@ -21,7 +21,7 @@ class UserLogin(UserBase):
 
 class UserResponse(BaseModel):
     fullname: str
-    email: str
+    email: EmailStr
     is_new_user: bool
     class Config:
         orm_mode = True
@@ -30,7 +30,7 @@ class UserUpdate(BaseModel):
     fullname: Optional[str] = None
     password: Optional[str] = None
     is_new_user: Optional[bool] = None
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
     gender: Optional[str] = None
     dob: Optional[str] = None
     class Config:
